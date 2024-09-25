58670f4b9e97f1d56476f9149646dc35df386030
Sebastvin <Sebastvin@gmail.com>
2024-09-25 22:30:21 +0200

    Setup DB

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from database import Base, engine
+import os
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, CheckConstraint
-from sqlalchemy.orm import relationship, validates
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
